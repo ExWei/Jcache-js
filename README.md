5d55db16aed5b82bd09f3f2a4ae3f7a3c5d9b7db
Alex Schafer <aexwei@gmail.com>
2013-12-26 19:08:30 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+Jcache
+=========
+Jcache is a node.js library for caching.
+## Installation
+You can install Jcache with the npm:
+```bash
+npm install jcache
+```
+
+## Functions
+### add(key, data)
+Add new data to cache
+
+__Arguments__
+
+* key - Key (ID) for access to cached data.
+* data - Value for caching.
+
+__Example__
+
+```js
+// Add data "10" with key "5+5" to cache
+var jcache = require('jcache');
+jcache.add("5+5", 10);
+```
+
+### get(key)
+Get data from cache.
+
+__Arguments__
+
+* key - Key (ID) for access to cached data.
+
+__Example__
+
+```js
+// Get data with id "5+5" from cache
+var jcache = require('jcache');
+jcache.get("5+5"); // Should return 10 if using with above example.
+```
+
+### exists(key)
+Check that data with key exists in cache
+
+__Arguments__
+
+* key - Key (ID) for access to cached data.
+
+__Example__
+
+```js
+// Check that data with key "5+5" exists in cache
+var jcache = require('jcache');
+jcache.exists("5+5"); // Should return true if using with above examples.
+```
+
+### remove(key)
+Remove data with key
+
+__Arguments__
+
+* key - Key (ID) for access to cached data.
+
+__Example__
+
+```js
+// Remove data with key "5+5"
+var jcache = require('jcache');
+jcache.remove("5+5");
+```
+
---
+++ test/jcache.js
@@ -0,0 +1,39 @@
+var should = require("should");
+var jcache = require("../lib/jcache.js");
+describe("Basic functions", function () {
+    describe("Add", function () {
+        it("Should return true", function () {
+            jcache.add("one", "oneValue").should.be.true;
+            jcache.add(2, 5).should.be.true;
+        });
+    });
+    describe("Get", function () {
+        it("Should return oneValue", function () {
+            jcache.get("one").should.be.equal("oneValue");
+            jcache.get(2).should.be.equal(5);
+        });
+    });
+    describe("Remove", function () {
+        it("Should remove value with id one", function () {
+            jcache.remove("one").should.equal(true);
+            jcache.remove(2).should.equal(true);
+        });
+        it("Should throw error when trying to get removed value", function () {
+            (function(){ jcache.get("one"); }).should.throw(/not exists/);
+        })
+    });
+    describe("Exists", function () {
+        it("Should return true", function() {
+            jcache.add("one", "oneValue");
+            jcache.add(2, 5);
+            jcache.exists("one").should.be.true;
+            jcache.exists(2).should.be.true;
+            jcache.remove("one");
+            jcache.remove(2);
+        });
+        it("Should return false", function() {
+            jcache.exists("someString").should.be.false;
+            jcache.exists(3).should.be.false;
+        });
+    });
+});
